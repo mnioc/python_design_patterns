e927250353711310f610f672d4a384a7d50bb0d4
JuqiPeng <juqi.peng@dorabot.com>
2022-03-04 11:14:39 +0800

    update builder

--- 创建型/builder.py
+++ 创建型/builder.py
@@ -13,63 +13,135 @@
 class Food:
     """食品类"""
 
-    def __init__(self) -> None:
-        self.price = 0
-
     @property
     def food_name(self):
         return self.__name__
 
 class Cola(Food):
-
     __name__ = "可乐"
+    price = 6
 
-    def __init__(self) -> None:
-        self.price = 6
+class Milk(Food):
+    __name__ = "牛奶"
+    price = 12
+
 
 class Chips(Food):
-
     __name__ = "薯条"
+    price = 18
 
-    def __init__(self) -> None:
-        self.price = 18
+
+class Chicken(Food):
+    __name__ = "鸡米花"
+    price = 20
+
 
 class Hamburg(Food):
-
     __name__ = "汉堡"
+    price = 35
 
-    def __init__(self) -> None:
-        self.price = 35
 
-class Application():
-    """点餐程序"""
+# class Application():
+#     """点餐程序"""
 
-    order = []
-    order.append(Chips())
-    order.append(Hamburg())
-    [print(food.food_name, ": ", food.price) for food in order]
-    print("总价: ", sum([food.price for food in order]))
+#     order = []
+#     order.append(Chips())
+#     order.append(Hamburg())
+#     [print(food.food_name, ": ", food.price) for food in order]
+#     print("总价: ", sum([food.price for food in order]))
 
 
 # ---------------------------------------------------------------------------------------------
-# 点餐系统v2.0, 代码看着low，臃肿，决定采用建造器模式进行重构, 并且订单需要根据食物类型进行分类统计，
-# 比如一个完整的订单需要包含饮料、主食、小吃三个部分，我们的第一版代码是没有实现的
+# 点餐系统v2.0, 代码看着low，臃肿，决定采用建造器模式进行重构, 并且套餐由多种食物类型构成，
+# 比如一个完整的套餐需要包含饮料、主食、小吃三个部分，我们的第一版代码是没有实现的
 
 from abc import ABC, abstractmethod
+from typing import Any
 
-class OrderBuilder(ABC):
+# 抽象套餐生成器
+class PackageBuilder(ABC):
+
+    @property
+    @abstractmethod
+    def package(self) -> None:
+        pass
 
     @abstractmethod
-    def add_staple(self):
+    def add_staple(self) -> None:
         raise NotImplementedError('`add_staple()` must be implemented.')
     
     @abstractmethod
-    def add_drink(self):
+    def add_drink(self) -> None:
         raise NotImplementedError('`add_staple()` must be implemented.')
     
     @abstractmethod
-    def add_snacks(self):
+    def add_snacks(self) -> None:
         raise NotImplementedError('`add_staple()` must be implemented.')
-    
 
-# class 
+
+class ConcreteBuilderPackage(PackageBuilder):
+
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self._package = Package()
+    
+    @property
+    def package(self) -> 'Package':
+        package = self._package
+        self.reset()
+        return package
+    
+    def add_staple(self, food: Food) -> None:
+        self._package.add("staple", food())
+    
+    def add_drink(self, food: Food) -> None:
+        self._package.add("drink", food())
+    
+    def add_snacks(self, food: Food) -> None:
+        self._package.add("snacks", food())
+
+
+# 套餐
+class Package:
+
+    def __init__(self):
+        self.staple = []
+        self.drink = []
+        self.snacks = []
+
+    def add(self, food_type: str, food: Any) -> None:
+
+        if food_type == "staple":
+            self.staple.append(food)
+        elif food_type == "drink":
+            self.drink.append(food)
+        elif food_type == "snacks":
+            self.snacks.append(food)
+
+    def show(self):
+        print("您的套餐包含: ")
+        staple = ",".join([food.food_name for food in self.staple])
+        print(f"\t主食类: {staple}")
+        drink = ",".join([food.food_name for food in self.drink])
+        print(f"\t饮料类: {drink}")
+        snacks = ",".join([food.food_name for food in self.snacks])
+        print(f"\t小吃类: {snacks}")
+        print(f"\t总价为: {sum([food.price for food in (self.snacks + self.drink + self.staple)])} 元")
+
+
+class Application:
+
+    def run(self):
+        # 一个套餐对象由多个部分组成，包括主食、小吃、饮料。。。
+        # 这样我们可以采用建造者模式，可以有效的避免类的实例化的时候需要进行大量的赋值
+        builder = ConcreteBuilderPackage()
+        builder.add_drink(Cola)
+        builder.add_staple(Hamburg)
+        builder.add_snacks(Chicken)
+        builder.add_snacks(Chips)
+        builder.package.show()
+
+Application().run()
+
